5027599e0712475ae2fe841475329385710114de
Nikolay Grozdanov <nagrozdanov@gmail.com>
2020-03-23 20:55:06 +0200

    initial commit

---
+++ src/main/java/com/nike/nordgym/NordgymBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nike.nordgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordgymBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordgymBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nike/nordgym/nordgymbackend/NordgymBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nike.nordgym.nordgymbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordgymBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
