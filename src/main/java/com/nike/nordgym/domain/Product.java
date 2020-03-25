af1446da012bbed4803fac1e4e327d97ab4cbef4
Kaloyan Gangov <kaloyan.gangov@mail.schwarz>
2020-03-25 10:49:28 +0200

    added Product Entity

---
+++ src/main/java/com/nike/nordgym/domain/Product.java
@@ -0,0 +1,23 @@
+package com.nike.nordgym.domain;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import javax.validation.constraints.DecimalMin;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    @DecimalMin(value = "0.01")
+    private BigDecimal price;
+}
